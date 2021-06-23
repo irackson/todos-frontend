4a4888105186700127355155005260e65aa84f90
irackson <nasirus@protonmail.com>
2021-06-22 22:49:16 -0400

    yarn add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Post from './components/Post';
 import AllPosts from './pages/AllPosts';
 import Form from './pages/Form';
-import SinglePost from './pages/SinglePost]';
+import SinglePost from './pages/SinglePost';
 import { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
@@ -18,7 +18,7 @@ function App() {
     const url = 'https://todos-rails-backend.herokuapp.com/todos/';
     const [posts, setPosts] = useState([]);
 
-    return <div className="App"></div>;
+    return <div className="App">app</div>;
 }
 
 export default App;
